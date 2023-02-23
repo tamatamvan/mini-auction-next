e4ba4cc4d98157879a22c101ef9d24532cef4d1c
Septian A Tama <sfirst.tama@gmail.com>
2023-02-23 22:29:47 +0700

    initiate profile page

---
+++ src/pages/profile.tsx
@@ -0,0 +1,12 @@
+import { type NextPage } from 'next';
+import PageHeading from '~/components/PageHeading';
+
+const ProfilePage: NextPage = () => {
+  return (
+    <>
+      <PageHeading>Profile</PageHeading>
+    </>
+  );
+};
+
+export default ProfilePage;
