8371cb6c69ffa067cd18a56d551377854ed5795f
Septian A Tama <sfirst.tama@gmail.com>
2023-02-27 00:53:37 +0700

    add deposit trpc procedure

--- src/server/api/routers/transactions.ts
+++ src/server/api/routers/transactions.ts
@@ -1,17 +1,30 @@
 import { z } from 'zod';
 import { createTRPCRouter, protectedProcedure } from '~/server/api/trpc';
 
-const TRX_TYPES = ['DEPOSIT', 'BID', 'REFUND'] as const;
-const TRX_STATUS = ['PENDING', 'REJECTED', 'COMPLETED'] as const;
-
 const depositObj = z.object({
   amount: z.number().positive(),
-  trxType: z.enum(TRX_TYPES),
-  trxStatus: z.enum(TRX_STATUS),
+  datetime: z.string().datetime(),
 });
 
 export const transactionsRouter = createTRPCRouter({
+  allTrxs: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.transaction.findMany({
+      where: {
+        userId: { equals: ctx.session.user.id },
+      },
+    });
+  }),
   deposit: protectedProcedure.input(depositObj).mutation(({ input, ctx }) => {
-    return { ...input, datetime: new Date(), userId: ctx.session.user.id };
+    const newTrx = ctx.prisma.transaction.create({
+      data: {
+        ...input,
+        datetime: new Date(),
+        userId: ctx.session.user.id,
+        trxType: 'DEPOSIT',
+        trxStatus: 'COMPLETED',
+      },
+    });
+
+    return newTrx;
   }),
 });
